8faf2c472a6dcf3970ca1645f2a8144e509a84f3
Philip Tong <philiptong@users.noreply.github.com>
2019-04-14 21:26:01 +0100

    Add testing api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'client';
+  users = '';
+
+  constructor(private httpclient: HttpClient) {}
+
+  getUsers(): void {
+    this.httpclient.get<string>('/api/users').subscribe(
+      result => {
+        this.users = JSON.stringify(result);
+      },
+      error => {
+        alert(error);
+      }
+    );
+  }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify([{ id: 1, name: 'philip' }]));
 });
 
 module.exports = router;
